7227003e88ff99b15f54b0aadd0e125a8db1fe72
Abdirahman04 <abdixcom@gmail.com>
2024-02-15 18:10:05 +0300

    added PostTransaction in transaction_handlers

---
+++ api/handlers/transaction_handler.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Abdirahman04/bytebank-api/models"
+	"github.com/Abdirahman04/bytebank-api/services"
+)
+
+func PostTransaction(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  var transaction models.TransactionRequest
+  err := json.NewDecoder(r.Body).Decode(&transaction)
+  if err != nil {
+    http.Error(w, "Bad request", http.StatusBadRequest)
+  }
+  res, err := services.SaveTransaction(transaction)
+  if err != nil {
+    http.Error(w, "Bad request", http.StatusBadRequest)
+  }
+  json.NewEncoder(w).Encode(res)
+}
