804d7dc4f74efb05a673833ed7424ee478a92231
Abdirahman04 <abdixcom@gmail.com>
2024-02-15 13:39:47 +0300

    added GetTransactions method in transaction_repository

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/Abdirahman04/bytebank-api/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func SaveTransaction(transaction models.Transaction) (string, error) {
@@ -15,4 +16,25 @@ func SaveTransaction(transaction models.Transaction) (string, error) {
     return "", err
   }
   return fmt.Sprintf("added %v", res.InsertedID), nil
+}
+
+func GetTransactions() ([]models.Transaction, error) {
+  client := Connect()
+  collection := client.Database("bytebank").Collection("transactions")
+  filter := bson.D{}
+  res, err := collection.Find(context.Background(), filter)
+  if err != nil {
+    return nil, err
+  }
+  res.Close(context.Background())
+  var transactions []models.Transaction
+  for res.Next(context.Background()) {
+    var transaction models.Transaction
+    err := res.Decode(&transaction)
+    if err != nil {
+      return nil, err
+    }
+    transactions = append(transactions, transaction)
+  }
+  return transactions, nil
 }
