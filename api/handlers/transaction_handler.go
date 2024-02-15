f37b21d6bff5d8e323eecb0c7e0a5a15f32e518b
Abdirahman04 <abdixcom@gmail.com>
2024-02-15 18:17:59 +0300

    added GetTransactionsByAccountId in transaction_handlers

--- api/handlers/transaction_handler.go
+++ api/handlers/transaction_handler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Abdirahman04/bytebank-api/models"
 	"github.com/Abdirahman04/bytebank-api/services"
+	"github.com/gorilla/mux"
 )
 
 func PostTransaction(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +28,26 @@ func GetTransactions(w http.ResponseWriter, r *http.Request) {
   res, err := services.GetTransactions()
   if err != nil {
     http.Error(w, "Bad request", http.StatusBadRequest)
+  }
+  json.NewEncoder(w).Encode(res)
+}
+
+func GetTransactionById(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  id := mux.Vars(r)["id"]
+  res, err := services.GetTransactionById(id)
+  if err != nil {
+    http.Error(w, "Bad Request", http.StatusBadRequest)
+  }
+  json.NewEncoder(w).Encode(res)
+}
+
+func GetTransactionsByAccountId(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  id := mux.Vars(r)["id"]
+  res, err := services.GetTransactionsByAccountId(id)
+  if err != nil {
+    http.Error(w, "Bad Request", http.StatusBadRequest)
   }
   json.NewEncoder(w).Encode(res)
 }
