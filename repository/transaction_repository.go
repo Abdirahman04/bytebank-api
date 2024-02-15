e4c64ef9c85a62fe6c9082ed38dab8e3ee7102ef
Abdirahman04 <abdixcom@gmail.com>
2024-02-15 13:46:53 +0300

    added GetTransactionByID method in transaction_repository

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Abdirahman04/bytebank-api/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func SaveTransaction(transaction models.Transaction) (string, error) {
@@ -26,7 +27,7 @@ func GetTransactions() ([]models.Transaction, error) {
   if err != nil {
     return nil, err
   }
-  res.Close(context.Background())
+  defer res.Close(context.Background())
   var transactions []models.Transaction
   for res.Next(context.Background()) {
     var transaction models.Transaction
@@ -37,4 +38,20 @@ func GetTransactions() ([]models.Transaction, error) {
     transactions = append(transactions, transaction)
   }
   return transactions, nil
+}
+
+func GetTransactionById(id string) (models.Transaction, error) {
+  client := Connect()
+  collection := client.Database("bytebank").Collection("transactions")
+  var transaction models.Transaction
+  objectId, err := primitive.ObjectIDFromHex(id)
+  if err != nil {
+    return transaction, err
+  }
+  filter := bson.M{"_id": objectId}
+  err = collection.FindOne(context.Background(), filter).Decode(&transaction)
+  if err != nil {
+    return transaction, err
+  }
+  return transaction, nil
 }
