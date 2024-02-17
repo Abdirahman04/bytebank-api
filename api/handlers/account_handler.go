a864b5e36b754e56a0d8e4119e9296f7a9558299
Abdirahman04 <abdixcom@gmail.com>
2024-02-17 18:04:20 +0300

    added some logs to PostAccount in account_handlers

--- api/handlers/account_handler.go
+++ api/handlers/account_handler.go
@@ -11,15 +11,16 @@ import (
 )
 
 func PostAccount(w http.ResponseWriter, r *http.Request) {
+  log.Println("PostAccount in handlers hit")
   w.Header().Set("Content-Type", "application/json")
   var account models.AccountRequest
   err := json.NewDecoder(r.Body).Decode(&account)
   if err != nil {
-    log.Fatal(err)
+    log.Println("Error decoding account", err)
   }
   res, err := services.PostAccount(account)
   if err != nil {
-    log.Fatal(err)
+    log.Println("Error saving account", err)
   }
   json.NewEncoder(w).Encode(res)
 }
